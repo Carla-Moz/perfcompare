bcd2d9984c3354c22ebd1f69412bc6bd565c5a16
Carla Severe <csevere@mozilla.com>
2023-03-15 18:53:37 -0400

    header, button and switch styles

--- src/components/Shared/beta/PerfCompareHeader.tsx
+++ src/components/Shared/beta/PerfCompareHeader.tsx
@@ -1,67 +1,56 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { style } from 'typestyle';
 
 import { Strings } from '../../../resources/Strings';
-import {
-  Spacing,
-  ButtonsLightRaw,
-  ButtonsDarkRaw,
-  Colors,
-} from '../../../styles';
+import { Spacing, ButtonsLightRaw, Colors } from '../../../styles';
 import ToggleDarkMode from './ToggleDarkModeButton';
 
 const strings = Strings.components.header;
 
-function PerfCompareHeader(props: PerfCompareHeaderProps) {
-  const { themeMode, toggleColorMode } = props;
-
-  const styles = {
-    container: style({
-      marginBottom: `${Spacing.layoutLarge + 20}px`,
-      padding: 0,
-      width: '100%',
-      minHeight: '357px',
-      backgroundColor:
-        themeMode == 'light' ? Colors.Background200 : Colors.Background200Dark,
-      backgroundImage: strings.bgLink,
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      backgroundPositionY: 'top',
-      $nest: {
-        '.header-text': {
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          maxWidth: '472px',
-          margin: '0 auto',
-        },
-        '.perfcompare-header': {
-          marginBottom: `${Spacing.Large}px`,
-        },
-        '.perfcompare-tagline': {
-          marginBottom: `${Spacing.xxLarge}px`,
-        },
-        '.MuiButtonBase-root': {
-          marginBottom: `${Spacing.layoutLarge + 14}px`,
-          maxWidth: '104px',
-          alignSelf: 'center',
-          ...(themeMode == 'light'
-            ? ButtonsLightRaw.Secondary
-            : ButtonsDarkRaw.Secondary),
-        },
-        '.learn-more-btn': {
-          //hidden until purpose is determined
-          //visibility: 'hidden',
-        },
+const styles = {
+  container: style({
+    margin: 0,
+    padding: 0,
+    width: '100%',
+    minHeight: '357px',
+    backgroundColor: Colors.Background200,
+    backgroundImage: strings.bgLink,
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    $nest: {
+      '.header-text': {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        maxWidth: '472px',
+        margin: '0 auto',
       },
-    }),
-  };
+      '.perfcompare-header': {
+        marginBottom: `${Spacing.Large}px`,
+      },
+      '.perfcompare-tagline': {
+        marginBottom: `${Spacing.xxLarge}px`,
+      },
+      '.MuiButtonBase-root': {
+        ...ButtonsLightRaw.Secondary,
+        marginBottom: `${Spacing.layoutLarge + 14}px`,
+        maxWidth: '104px',
+        alignSelf: 'center',
+      },
+      '.learn-more-btn': {
+        //hidden until purpose is determined
+        visibility: 'hidden',
+      },
+    },
+  }),
+};
+
+function PerfCompareHeader() {
   return (
-    <Grid className={`header-container ${styles.container}`}>
-      <ToggleDarkMode theme={themeMode} toggleColorMode={toggleColorMode} />
+    <div className={`header-container ${styles.container}`}>
+      <ToggleDarkMode />
       <Box className='header-text'>
         <Typography
           variant='h1'
@@ -82,13 +71,8 @@ function PerfCompareHeader(props: PerfCompareHeaderProps) {
         </Typography>
         <Button className='learn-more-btn'>{strings.learnMore}</Button>
       </Box>
-    </Grid>
+    </div>
   );
-}
-
-interface PerfCompareHeaderProps {
-  themeMode: 'light' | 'dark';
-  toggleColorMode: () => void;
 }
 
 export default PerfCompareHeader;
