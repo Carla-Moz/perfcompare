428f2a4341374702173f694a25012677d7980a63
Carla Severe <csevere@mozilla.com>
2023-03-20 18:46:17 -0400

    added beta search route

--- src/__tests__/Search/SearchViewBeta.test.tsx
+++ src/__tests__/Search/SearchViewBeta.test.tsx
@@ -1,24 +1,15 @@
-import { renderHook } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 
 import SearchViewBeta from '../../components/Search/beta/SearchView';
-import useProtocolTheme from '../../theme/protocolTheme';
 import { renderWithRouter } from '../utils/setupTests';
+import { screen } from '../utils/test-utils';
 
 describe('Search View', () => {
-  const protocolTheme = renderHook(() => useProtocolTheme()).result.current
-    .protocolTheme;
-
-  const toggleColorMode = renderHook(() => useProtocolTheme()).result.current
-    .toggleColorMode;
-
   it('renders correctly when there are no results', async () => {
-    renderWithRouter(
-      <SearchViewBeta
-        toggleColorMode={toggleColorMode}
-        protocolTheme={protocolTheme}
-      />,
-    );
+    renderWithRouter(<SearchViewBeta />);
+
+    // Title appears
+    expect(screen.getByText(/PerfCompare/i)).toBeInTheDocument();
 
     expect(document.body).toMatchSnapshot();
     await act(async () => void jest.runOnlyPendingTimers());
